27d02bbdc4d6a3054276006b4c1bf6f457ea8b09
Atrem_ <dwg5@ukr.net>
2024-01-17 20:03:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [photos, setPhotos] = useState([]),
+    [currentPage, setCurrentPage] = useState(1),
+    [fetching, setFetching] = useState(true),
+    [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    if (fetching) {
+      console.log('fetching')
+      axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`)
+        .then(respons => {
+          setPhotos([...photos, ...respons.data]);
+          setCurrentPage(prevState => prevState + 1);
+          setTotalCount(respons.headers['x-total-count'])
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching, currentPage]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }, [totalCount]);
+
+  const scrollHandler = (e) => {
+    if (e.target.documentElement.scrollHeight
+      - (e.target.documentElement.scrollTop + window.innerHeight) < 100
+      && photos.length < totalCount) {
+      setFetching(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+      {photos.map(photos => (
+        <div className="photo" key={photos.id}>
+          <div className="title">{photos.id}.) {photos.title}</div>
+          <img src={photos.thumbnailUrl} alt="img for example" />
+        </div>
+      ))}
     </div>
   );
 }
